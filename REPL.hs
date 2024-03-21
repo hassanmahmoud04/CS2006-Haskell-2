b811277ef2c106dd7400b46f892921deb242e63d
Hassan Mahmoud <hm253@st-andrews.ac.uk>
2024-03-21 15:35:28 +0000

    add updatevars

--- REPL.hs
+++ REPL.hs
@@ -12,13 +12,17 @@ initLState = LState []
 -- that name and value added.
 -- If it already exists, remove the old value
 updateVars :: Name -> Int -> [(Name, Int)] -> [(Name, Int)]
-updateVars = undefined
+updateVars name val [] = [(name, val)]
+updateVars name val ((k,v):xs) = if name == k
+                                    then ((k,val):xs)
+                                    else updateVars name val xs
 
 -- Return a new set of variables with the given name removed
 dropVar :: Name -> [(Name, Int)] -> [(Name, Int)]
 dropVar = undefined
 
 process :: LState -> Command -> IO ()
+<<<<<<< Updated upstream
 process st (Set var e) 
      = do let st' = undefined
           -- st' should include the variable set to the result of evaluating e
@@ -28,6 +32,29 @@ process st (Print e)
           -- Print the result of evaluation
           repl st'
 process st Quit = do return ()
+=======
+process st (Set var e) = do 
+     let val = eval (vars st) e
+     case val of
+          Nothing -> do
+               putStrLn ("Error: ")
+               repl st
+          Just val -> do
+               let st' = LState ( updateVars var val (vars st))
+               repl st'
+     -- st' should include the variable set to the result of evaluating e
+          
+process st (Print e) = do
+    let evalResult = eval (vars st) e
+    case evalResult of
+        Just val -> do
+               putStrLn (show val)
+               repl st
+        Nothing -> do
+               putStrLn ("Error: ")
+               repl st
+          
+>>>>>>> Stashed changes
 
 -- Read, Eval, Print Loop
 -- This reads and parses the input using the pCommand parser, and calls
